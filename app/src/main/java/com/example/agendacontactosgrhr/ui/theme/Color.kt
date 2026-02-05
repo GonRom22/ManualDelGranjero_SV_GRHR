acbc3588624706b63d7e5be2819ea50e12913cfc
hr <hector_ronquillo@hotmail.com>
2026-02-05 15:54:29 +0100

    Creación de una clase alternativa kontaktoviewmodel en la que se ha añadido el crud y el sqp manuela de los personajes de stardew oara revisar en conjunto Añadidos paleta de colores y fuente

--- app/src/main/java/com/example/agendacontactosgrhr/ui/theme/Color.kt
+++ app/src/main/java/com/example/agendacontactosgrhr/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//añadimos colores de Stardew:
+val StardewNaranja = Color(0xFFE89a56) //Botones y cajas
+val StardewMarron = Color(0xFF5C2D16) //Bordes y texto oscuro
+val StardewCrema = Color(0xFFFFE6A8) //Color para papeles e interfaz
+val StardewAzul = Color(0xFF3866CF) //Botones de accíon
